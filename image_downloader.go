69407e62c68dd3324b970d4e56adfcf6ea10a2f9
Sepehr Saeidi Farzad <ssaeidifarzad@gmail.com>
2024-06-09 17:23:58 +0330

    feat: database connection implemented

--- image_downloader.go
+++ image_downloader.go
@@ -1,13 +1,52 @@
 package main
 
-import "gorm.io/gorm"
+import (
+	"os"
+	"sync"
 
-type DBconn struct {
-	db *gorm.DB
-}
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var (
+	dbonce sync.Once
+	db     *gorm.DB
+)
 
 type Image struct {
 	gorm.Model
 	data   []byte
 	format string
+}
+
+func GetDB() *gorm.DB {
+	dbonce.Do(func() {
+		gormconn, err := ConnectDB()
+		if err != nil {
+			panic(err)
+		}
+		db = gormconn
+	})
+	return db
+}
+
+func ConnectDB() (*gorm.DB, error) {
+	err := godotenv.Load()
+	if err != nil {
+		return nil, err
+	}
+	var (
+		DbName   = os.Getenv("POSTGRES_DB")
+		User     = os.Getenv("POSTGRES_USER")
+		Password = os.Getenv("POSTGRES_PASSWORD")
+		Host     = os.Getenv("POSTGRES_HOST")
+		Port     = os.Getenv("POSTGRES_PORT")
+	)
+	dsn := "host=" + Host + " user=" + User + " password=" + Password + " dbname=" + DbName + " port=" + Port + " sslmode=disable TimeZone=Asia/Tehran"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
 }
